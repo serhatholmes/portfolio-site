8ff5b0268d0d2af21f5c34ef95f1b49b76167fdb
Serhat Pehlivan <serhat9703@hotmail.com>
2023-01-22 13:21:41 +0300

    Mark3-ContactPage is done and form is working

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Main from "./components/Main";
 import SideNav from "./components/Sidenav";
 import Work from "./components/Work";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Main />
       <Work />
       <Projects />
+      <Contact />
     </div>
   );
 }
